2bf9ab7563f9275e258e20d79f3655b0158e423a
Sophia Farr <sophia.farr.427@gmail.com>
2020-10-09 12:16:45 -0700

    Added README Added test for string containing an `a` followed by zero or more `b`

--- test_regular_expressions.py
+++ test_regular_expressions.py
@@ -9,5 +9,12 @@ def test_string_for_characters(test_string, expected):
     assert match.group(0) == expected
 
 
+@pytest.mark.parametrize("test_string,expected", [('a', 'a'), ('_', None),
+                                                  ('ab', 'ab'), ('abb', 'abb'),
+                                                  ('abba', 'abb')])
+def test_string_a_followed_by_zero_or_more_b(test_string, expected):
+    assert re.match(r'a[b]*', test_string) == expected
+
+
 if __name__ == '__main__':
     pytest.main()
