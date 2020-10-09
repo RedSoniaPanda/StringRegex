97b2083c8a524496f941a98644ca3c6d784532c5
Sophia Farr <sophia.farr.427@gmail.com>
2020-10-09 13:12:57 -0700

    Added test to find string with an `a` followed by one or more `b`

--- test_regular_expressions.py
+++ test_regular_expressions.py
@@ -21,5 +21,17 @@ def test_string_a_followed_by_zero_or_more_b(test_string, expected):
         assert match.group(0) == expected
 
 
+@pytest.mark.parametrize("test_string,expected", [('a', ''), ('_', ''),
+                                                  ('abb', 'abb'), ('cb', ''),
+                                                  ('abbc', 'abb'), ('abba', 'abb'),
+                                                  ('.', '')])
+def test_string_a_followed_by_one_or_more_b(test_string, expected):
+    match = re.match(r'ab+', test_string)
+    if not match:
+        assert '' == expected
+    else:
+        assert match.group(0) == expected
+
+
 if __name__ == '__main__':
     pytest.main()
